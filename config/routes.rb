19cb486088205b3dd21db4a5f0e8f928fa98ad81
Jean-Philippe-94 <johnanag94@gmail.com>
2020-09-22 22:56:07 +0200

    chap12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
  
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'users/new'
   get 'users/create'
   get 'users/show'
@@ -15,5 +17,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
